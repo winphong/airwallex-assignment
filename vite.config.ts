a11fdb1a1f314fe99d18d90423cc6b5faa338ae7
winphong <winphong1011@gmail.com>
2024-12-18 02:17:35 +0800

    chore: setup test environment, revert vite@5 latest due to vitest not supporting vite@6

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "node_modules/**"],
+    setupFiles: "./test/vitest.setup.ts",
+  },
 });
